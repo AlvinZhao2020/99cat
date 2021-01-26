48de3caee645c13c0617dbbba0e01e5b4f9507d9
alvin zhao <yibozhao2018@gmail.com>
2021-01-26 13:07:39 -0800

    seed some cats

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,18 @@
 class CatsController < ApplicationController
+    def index
+        @cats = Cat.all
+        render :index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def updated
+    end
+
+    def destroy
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cat.destroy_all
+c1 = Cat.create(birth_date:'2020/2/2', name:'lux',color:'WHITE',sex:'M')
+c2 = Cat.create(birth_date:'2010/2/2', name:'goo',color:'WHITE',sex:'F')
+c3 = Cat.create(birth_date:'2011/2/2', name:'lee',color:'WHITE',sex:'F')
+c4 = Cat.create(birth_date:'2015/2/2', name:'mike',color:'WHITE',sex:'M')
