ff76cdabad1bf4c65e3060facbc893ddaf8cf432
alvin zhao <yibozhao2018@gmail.com>
2021-01-26 12:53:31 -0800

    setup cat model

---
+++ app/models/cat.rb
@@ -0,0 +1,19 @@
+    # t.date "birth_date", null: false
+    # t.string "name", null: false
+    # t.string "color", null: false
+    # t.string "sex", null: false
+    # t.text "description"
+    # t.datetime "created_at", null: false
+    # t.datetime "updated_at", null: false
+require 'action_view'
+
+class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper
+    CAT_COLORS = %W(BLACK WHITE ORANGE BROWN).freeze
+    validates :color, inclusion: CAT_COLORS
+    validates :sex, inclusion: %w(M F)
+    validates :birth_date, :name, :color, :sex, presence: true
+    def age
+        time_ago_in_words(birth_date)
+    end
+end
